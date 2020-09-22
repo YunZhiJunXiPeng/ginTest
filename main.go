f1b1260b5496d071d0a504c9bfa8c93f77c0b325
SmallSuperMan <630239746@qq.com>
2020-09-22 18:48:12 +0800

    20

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	// "log"
+	// "net/http"
+	// "path"
+	// "path/filepath"
+
+	// "github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Printf("你好，世界！")
+	test()
+	// router := gin.Default()
+	// router.POST("/upload", func(c *gin.Context) {
+	// 	// 从表单取文件
+	// 	file, err := c.FormFile("file")
+	// 	if err != nil {
+	// 		c.String(http.StatusBadRequest, "Bad request")
+	// 		return
+	// 	}
+	// 	files := file.Filename
+	// 	paths, fileName := filepath.Split(files)
+	// 	fmt.Println(paths, fileName)      //获取路径中的目录及文件名
+	// 	fmt.Println(filepath.Base(files)) //获取路径中的文件名
+	// 	fmt.Println(path.Ext(files))      //获取路径中的文件的后缀
+	// 	// 打印文件名称包括路径
+	// 	log.Println(file.Filename)
+	// 	// 上传到项目根目录  名字不变
+	// 	c.SaveUploadedFile(file, fileName)
+	// 	// 打印信息
+	// 	c.String(http.StatusCreated, fmt.Sprintf("'%s' upload!!!!!!!!!", file.Filename))
+	// })
+	// router.Run(":8000")
+}
---
+++ test.go
@@ -0,0 +1,24 @@
+package main
+
+/**
+** @ Author       : SmallSuperMan
+** @ Date         : 2020-09-16 00:28:31
+** @ LastEditTime : 2020-09-22 18:46:32
+** @ LastEditors  : SmallSuperMan
+** @ FilePath     : \ginTest\test.go
+** @ Description  :
+**/
+
+import (
+	"fmt"
+	"path"
+	"path/filepath"
+)
+
+func test() {
+	pathFileString := "C:/目录/文件名.后缀名"
+	paths, fileName := filepath.Split(pathFileString)
+	fmt.Println(paths, fileName)               // 获取路径中的目录及文件名
+	fmt.Println(filepath.Base(pathFileString)) // 获取路径中的文件名
+	fmt.Println(path.Ext(pathFileString))      // 获取路径中的文件的后缀
+}
