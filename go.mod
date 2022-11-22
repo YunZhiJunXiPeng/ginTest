8f385cf38152cda3fcc9adb61c12f8f11584fdd0
SuperOutMan <630239746@qq.com>
2022-11-23 01:05:34 +0800

    修改

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module smallsuperman/ginTest
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/thinkerou/favicon v0.1.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /**
 ** @ Author       : SmallSuperMan
 ** @ Date         : 2020-09-07 23:39:56
-** @ LastEditTime : 2022-11-23 00:50:41
+** @ LastEditTime : 2022-11-23 01:04:01
 ** @ LastEditors  : SmallSuperMan
 ** @ FilePath     : \ginTest\main.go
 ** @ Description  :
@@ -12,13 +12,17 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/thinkerou/favicon"
 )
 
 func main() {
 	fmt.Printf("你好,世界！\n")
 	// 创建服务
 	ginServer := gin.Default()
-	// 访问地址
+	// 设置图标
+	ginServer.Use(favicon.New("./shezhi.ico"))
+
+	// 访问地址 处理请求
 	ginServer.GET("/hello", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"msg": "你好世界"})
 	})
